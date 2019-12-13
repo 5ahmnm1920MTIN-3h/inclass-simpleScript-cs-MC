0819b9773cc948c1c280f584d9e20144860a3853
MC <marc.leitner@htl-salzburg.ac.at>
2019-12-13 19:00:34 +0100

    push git ignore

---
+++ inclass-simpleScript-cs-MC/Assets/MY Game/Scripts/ComputeSub.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ComputeSub : MonoBehaviour
+{
+    public Text result;
+    public InputField inputField1;
+    public InputField inputField2;
+    public Button resetButton;
+    public Button addButton;
+
+
+    public void SetResult()
+    {
+        result.text = AddNumbers().ToString();
+        inputField1.interactable = false;
+        inputField2.interactable = false;
+        addButton.interactable = false;
+        resetButton.interactable = true;
+    }
+
+    public void Reset()
+    {
+        inputField1.text = "0";
+        inputField2.text = "0";
+        inputField1.interactable = true;
+        inputField2.interactable = true;
+        addButton.interactable = true;
+        resetButton.interactable = false;
+        result.text = "Result";
+    }
+    private float AddNumbers()
+    {
+        float tempResult = float.Parse(inputField1.text) + float.Parse(inputField2.text);
+        return tempResult;
+    }
+
+
+    private float SubNumbers()
+    {
+        float tempResult = float.Parse(inputField1.text) - float.Parse(inputField2.text);
+        return tempResult;
+    }
+
+    public void OnClickSub()
+    {
+        result.text = SubNumbers().ToString();
+        Debug.Log(SubNumbers());
+    }
+
+}
